973199676d2468b078a2cff160207f7141c19b94
fcassinENSPS <66281325+fcassinENSPS@users.noreply.github.com>
2021-08-30 16:15:15 +0200

    fin

--- Hamilton.py
+++ Hamilton.py
@@ -7,9 +7,7 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 class Hamilton:
-    def __init__(self,eta,A,rho,nbp,M,dx,dy,T,dt,choixInt,choixInitialisation,choixOrdreGyro,choixOrdreFLR,ntheta):
-        # PARSING INPUTS 
-        #self.B = B 			# champ magnétique B
+    def __init__(self,eta,A,rho,nbp,M,T,dt,choixInt,choixInitialisation,choixOrdreGyro,choixOrdreFLR):
         self.A = A
         self.Ab = A*spc.jv(0,rho*np.sqrt(2))		# amplitude Phi modifier
         self.rho = rho 		# rayon Larmor
@@ -34,8 +32,6 @@ class Hamilton:
         #self.y[:] = np.random.rand(nbp)*2*np.pi    #random
         self.T = T 				# Temps final
         self.dt = dt			# Nombre iteration dans le temps
-        self.dx = dx		# taille maille x
-        self.dy = dy 		# taille maille y 
         self.choixInt = choixInt
         self.choixOrdreGyro = choixOrdreGyro
         self.eta = eta
@@ -51,19 +47,10 @@ class Hamilton:
             self.M =(M-1)/2
 
     def RungeKutta(self,Model,u0,Tf,dt):
-        #u = np.zeros((2*self.nbp,int(Tf/dt)+1))
         u = np.zeros((2*self.nbp,self.n+1))
         s = np.zeros((2*self.nbp,self.N+1))
         u[:,0] = u0
         s[:,0] = u0
-        """
-        for i in tqdm(range(int(Tf/dt))):
-            du0 = Model(dt*i,u[:,i])
-            du1 = Model(dt*(i+1/2),u[:,i]+dt*du0/2)
-            du2 = Model(dt*(i+1/2),u[:,i]+dt*du1/2)
-            du3 = Model(dt*(i+1),u[:,i]+dt*du2)
-            u[:,i+1] = u[:,i] + dt*(du0+2*du1+2*du2+du3)/6
-        """
         for i in tqdm(range(self.N)):
             for j in range(self.n):
                 du0 = Model(dt*j,u[:,j])
@@ -84,8 +71,6 @@ class Hamilton:
         alpha = 1+2*np.cos((self.M+1)*t)*spc.eval_chebyu(self.M-1,np.cos(t))
         beta =2*np.cos(self.M*t)*spc.eval_chebyu(self.M-1,np.cos(t))
 
-        #dphidy = 2*np.pi*self.A*(beta*sinx*cosy-alpha*cosx*siny)   #avec 2pi
-        #dphidx = 2*np.pi*self.A*(beta*cosx*siny-alpha*sinx*cosy)   #avec 2pi
         dphidy = self.A*(beta*sinx*cosy-alpha*cosx*siny)
         dphidx = self.A*(beta*cosx*siny-alpha*sinx*cosy)
 
@@ -100,8 +85,6 @@ class Hamilton:
         alpha = 1+2*np.cos((self.M+1)*t)*spc.eval_chebyu(self.M-1,np.cos(t))
         beta =2*np.cos(self.M*t)*spc.eval_chebyu(self.M-1,np.cos(t))
 
-        #dphidy = 2*np.pi*self.Ab*(beta*sinx*cosy-alpha*cosx*siny)  #avec 2pi
-        #dphidx = 2*np.pi*self.Ab*(beta*cosx*siny-alpha*sinx*cosy)  #avec 2pi
         dphidy = self.Ab*(beta*sinx*cosy-alpha*cosx*siny)  
         dphidx = self.Ab*(beta*cosx*siny-alpha*sinx*cosy)
 
@@ -117,8 +100,6 @@ class Hamilton:
         alpha = 1+2*np.cos((self.M+1)*t)*spc.eval_chebyu(self.M-1,np.cos(t))
         beta =2*np.cos(self.M*t)*spc.eval_chebyu(self.M-1,np.cos(t))
 
-        #dphidy = -2*np.pi*self.Ab*(beta*sinx*cosy-alpha*cosx*siny) + 2*(2*np.pi*self.A)**2*self.eta*((alpha**2+beta**2)*np.cos(2*x)*siny*cosy-2*alpha*beta*sinx*cosx*np.cos(2*y))  #avec 2pi
-        #dphidx = 2*np.pi*self.Ab*(beta*cosx*siny-alpha*sinx*cosy) - 2*(2*np.pi*self.A)**2*self.eta*((alpha**2+beta**2)*np.cos(2*y)*sinx*cosx-2*alpha*beta*siny*cosy*np.cos(2*x))   #avec 2pi
         dphidy = -self.Ab*(beta*sinx*cosy-alpha*cosx*siny) + 2*(self.A)**2*self.eta*((alpha**2+beta**2)*np.cos(2*x)*siny*cosy-2*alpha*beta*sinx*cosx*np.cos(2*y))
         dphidx = self.Ab*(beta*cosx*siny-alpha*sinx*cosy) - 2*(self.A)**2*self.eta*((alpha**2+beta**2)*np.cos(2*y)*sinx*cosx-2*alpha*beta*siny*cosy*np.cos(2*x))
 
@@ -202,8 +183,6 @@ class Hamilton:
             self.n = n
             s = self.RungeKutta(Modelx,X0,self.T,self.dt)
 
-            #x = s[:self.nbp,0:N*n:n]
-            #y = s[self.nbp:,0:N*n:n]
             x = s[:self.nbp,:]
             y = s[self.nbp:,:]
             x=np.transpose(x)
@@ -211,37 +190,14 @@ class Hamilton:
             print(x.shape)
 
         nTrapped = 0
-        #indice = []
-        #xTrapped = x.copy()
-        #yTrapped = x.copy()
         for i in range(self.nbp):
             if np.all((np.abs(x[0,i]-x[:,i])<2*np.pi)):
                 nTrapped+=1
-                #xTrapped[:,i] = x[:,i]
-                #yTrapped[:,i] = y[:,i]
-            #else:
-                #xTrapped[:,i] = x[int(np.random.rand(1)*self.N),i]
-                #yTrapped[:,i] = y[int(np.random.rand(1)*self.N),i]
-        #print('nTrapped/N')
-        #print(nTrapped/self.nbp)
-        """
-        xTrapped = np.zeros((self.N+1,nTrapped))
-        yTrapped = xTrapped.copy()
-        for i in range(nTrapped):
-            xTrapped[:,i] = x[:,indice[i]]
-            yTrapped[:,i] = y[:,indice[i]]
-        """
         
         r2 = np.zeros(N)
         for i in tqdm(range(N)):
             r2[i] = 1/(self.T-i*self.dt)*np.mean(np.sum((x[0+i:N]-x[0:N-i])**2+(y[0+i:N]-y[0:N-i])**2,axis=1))
         t = np.arange(N)*2*np.pi
         slope, intercept, r, p, se = linregress(t, r2)
-        
-        #slope = 0
-        #print('slope')
-        #print(slope)
-        #plt.plot(t,r2)
-        #plt.savefig('r2.png')
 
         return x,y,nTrapped/self.nbp,slope
